166a58b08c369cfd9d9e0f778ddcd2e48d277afe
Eleonora-Avramov <eleonorapetrovska@gmail.com>
2025-07-14 14:33:41 +0200

    prep weather add

---
+++ models/prep/prep_weather_hourly.sql
@@ -0,0 +1,29 @@
+WITH hourly_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_hourly')}}
+),
+add_features AS (
+    SELECT *
+		, timestamp::DATE AS date               -- only date (hours:minutes:seconds) as DATE data type
+		, timestamp:: AS time                           -- only time (hours:minutes:seconds) as TIME data type
+        , TO_CHAR(timestamp,'HH24:MI') as hour  -- time (hours:minutes) as TEXT data type
+        , TO_CHAR(timestamp, 'FMmonth') AS month_name   -- month name as a TEXT
+        , TO_CHAR (timestamp, 'FMday') weekday        -- weekday name as TEXT        
+        , DATE_PART('day', timestamp) AS date_day
+		, DATE_PART ('month' , timestamp) AS date_month
+		, DATE_PART ('year' , timestamp) AS date_year
+		, DATE_PART ('week' , timestamp) AS cw
+    FROM hourly_data
+),
+add_more_features AS (
+    SELECT *
+		,(CASE 
+			WHEN time >= '23:00:00' OR time < '06:00:00' THEN 'night'
+            WHEN time >= '06:00:00' AND time < '18:00:00' THEN 'day'
+            WHEN time >= '18:00:00' AND time < '23:00:00' THEN 'evening'
+		END) AS day_part
+    FROM add_features
+)
+
+SELECT *
+FROM add_more_features
