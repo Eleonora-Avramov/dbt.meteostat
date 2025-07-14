46c4f7ab157ffef0feaf65c779dcb7b5541fe1e4
Eleonora-Avramov <eleonorapetrovska@gmail.com>
2025-07-14 13:13:55 +0200

    new table

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,32 @@
+-- File: prep_weather_daily.sql
+
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ ref('staging_weather_daily') }}
+),
+
+add_features AS (
+    SELECT *
+        , DATE_PART('day', date) AS date_day          -- number of the day of month
+        , DATE_PART('month', date) AS date_month      -- number of the month of year
+        , DATE_PART('year', date) AS date_year        -- number of the year
+        , DATE_PART('week', date) AS cw               -- calendar week number
+        , TO_CHAR(date, 'Month') AS month_name        -- full month name
+        , TO_CHAR(date, 'Day') AS weekday             -- full weekday name
+    FROM daily_data
+),
+
+add_more_features AS (
+    SELECT *
+        , CASE 
+            WHEN DATE_PART('month', date) IN (12, 1, 2) THEN 'winter'
+            WHEN DATE_PART('month', date) IN (3, 4, 5) THEN 'spring'
+            WHEN DATE_PART('month', date) IN (6, 7, 8) THEN 'summer'
+            WHEN DATE_PART('month', date) IN (9, 10, 11) THEN 'autumn'
+          END AS season
+    FROM add_features
+)
+
+SELECT *
+FROM add_more_features
+ORDER BY date;
